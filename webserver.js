e963611aaf20d3e10b0f2a1bb2b109b53e9a95c7
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-30 15:28:36 +0900

    async await으로 재작성

--- webserver.js
+++ webserver.js
@@ -2,10 +2,12 @@ const http = require('http');
  
 const hostname = '127.0.0.1';
 const port = 1338;
- 
-http.createServer((req, res) => {
- res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello World!\n');
-}).listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`);
+
+const server = http.createServer(async(req, res) => {
+const write = await res.writeHead(200, { 'Content-Type': 'text/plain' });
+const result = write.end('Hello World!!!\n');
+return result;
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
